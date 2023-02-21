994a0f7b34a817ef55f455ac038f0a924e4ab3d0
lukkymaru <qua.nyan2ify@gmail.com>
2023-02-21 17:18:07 +0900

    ✨ TwitterCardのワイヤフレームを作成開始

---
+++ src/components/TwitterCard.tsx
@@ -0,0 +1,23 @@
+export const TwitterCard = () => {
+    return (
+
+        /*
+         rootContainer
+           color:black;
+           padding-top: 5rem( -> 20px); 
+           padding-bottom: 20rem( -> 80px);
+           display: flex;
+           justify-content: center;
+        */
+
+        <div className="bg-gray-200 pt-20 pb-80 flex justify-center">
+
+            <div className="bg-white border-gray-200 p-4 rounded-xl border max-w-xl min-w-[480px]">
+                {/* 本文 */}
+                <div className="text-black" >
+                TwitterCard
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next";
+import { TwitterCard } from "src/components/TwitterCard";
+
+//宣言
+// interface Foo  {
+//   a: number
+// }
+
+//(継承)(ここでエラーとなる)
+// interface Bar extends Foo {
+//   b: string;
+// }
+
+//利用(foo.a が、never型となり、使えない)(ここでエラーとなる)
+// const foo: Bar = {
+//   a: 1,
+// };
 
 const Home: NextPage = () => {
-  return <div>test</div>;
+    return (
+        <TwitterCard />
+    );
 };
 
 export default Home;
